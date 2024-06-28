b75b5aa772fc9789452fb72dc17f822f4d7fb076
Terrance Finley <112042894+TerranceFinleyZ@users.noreply.github.com>
2024-06-27 19:05:36 -0700

    Add files via upload

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,16 @@
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <span>
+          @
+        </span>
+        <button>
+            Get Website
+        </button>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/components/pages/Home.tsx
@@ -0,0 +1,22 @@
+import './Home.css'
+
+const Home = () => {
+  return (
+    <div className='home'>
+        <section className='home-container'>
+            <div className='home-text'>
+                <h1>
+                    MEZA.
+                </h1>
+                <div className='footer-home'>
+                    <p>LLC®</p>
+                    <p>Discover a new perspectives!</p>
+                </div>
+                <div className='gradient' />
+            </div>
+        </section>    
+    </div>
+  )
+}
+
+export default Home
