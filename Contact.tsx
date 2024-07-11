6b3e4c4950f9729fb05e582d7483fc10fc1ff1b6
Terrance Finley <112042894+TerranceFinleyZ@users.noreply.github.com>
2024-07-11 15:44:07 -0700

    Add files via upload

---
+++ Contact.tsx
@@ -0,0 +1,42 @@
+import './Contact.css'
+
+const Contact = () => {
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "c60b8564-eb14-4ae0-a1e2-b52e387b4ea5");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      
+    }
+  };
+  
+  return (
+    <div className='con'>
+      <form onSubmit={onSubmit}>
+        <h5>Get a Website!</h5>
+        <input type='text' id='firstName' placeholder='First Name' name='name' required />
+        <input type='text' id='lastName' placeholder='Last name' name='last name' required />
+        <input type='email' id='email' placeholder='Email' name='email' required />
+        <input type='text' id='phone' placeholder='Phone' name='phone' required />
+        <textarea name='message' required>Message Us...</textarea>
+        <input type='submit' value='Send' id='button' />
+      </form>
+    </div>
+  )
+}
+
+export default Contact
